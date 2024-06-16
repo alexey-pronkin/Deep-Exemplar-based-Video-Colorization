9d851c0d0e31b33cbc14d37feeedde1d06c21dc6
Pronkin Alexey <31882933+alexey-pronkin@users.noreply.github.com>
2024-06-16 22:51:13 +0300

    fix

--- run_png.sh
+++ run_png.sh
@@ -5,7 +5,8 @@ do
 python test.py --clip_path ./sample_videos7/clips/$n \
                --ref_path ./sample_videos7/ref/$n \
                --output_path ./sample_videos7/output \
-               --image_size [768,1024] \
-               --image_format png \
+               --image_size "[768,1024]" \
+               --image_format "png" \
+               --png_bits "uint16" \
                --make_video
 done
