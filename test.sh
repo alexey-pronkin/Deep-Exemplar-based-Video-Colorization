7e062a6a32228355bbc2838f033ab49e951611f4
Aleksei Pronkin <31882933+alexey-pronkin@users.noreply.github.com>
2024-12-19 07:58:48 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+python test.py --clip_path ./sample_videos7/clips/v04 \
+               --output_path ./sample_videos7/output \
+               --device "cpu" \
+               --image_size "[768,1024]" \
+               --image_format "png" \
+               --png_bits "uint16" \
+               --make_video
